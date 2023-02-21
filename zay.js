55d49210a3339db32f997f63d987f7bddfa4c2e2
moszay111 <mossmoses2000@gmail.com>
2023-02-21 13:29:25 +0100

    added some codes

---
+++ zay.js
@@ -0,0 +1,8 @@
+let country = "Portugal";
+let continent = "Europe";
+let population = "10";
+console.log(country, continent, population);
+
+let island = false;
+let language;
+console.log(typeof island, typeof population, typeof country);
