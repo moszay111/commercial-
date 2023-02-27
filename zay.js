c3ba679f342e4f30712fef8592a54366ad116120
moszay111 <mossmoses2000@gmail.com>
2023-02-27 19:29:45 +0100

    added

--- zay.js
+++ zay.js
@@ -1,27 +1,2 @@
-let country = "Portugal";
-let continent = "Europe";
-let population = "10";
-console.log(country, continent, population);
-
-let island = false;
-let language;
-console.log(typeof island, typeof population, typeof country);
-
-language = "Yoruba";
-const name = "Akpowu";
-const surname = "Arubi";
-
-console.log(population / 2);
-population++;
-console.log(population > 6);
-console.log(population < 33);
-
-const description = `${country} is in ${continent} and ${population} million people speak ${language}`;
-
-if (population > 33) {
-  console.log(`${country}'s population is above average`);
-} else {
-  console.log(
-    `${country}'s population is ${33 - population} million below average`
-  );
-}
+const name = "Moses";
+const surname = "Akpowu";
