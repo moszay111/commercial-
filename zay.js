77242457eee1492ac4e52af2dd65ef1886338cc6
moszay111 <mossmoses2000@gmail.com>
2023-02-27 19:19:02 +0100

    added code

--- zay.js
+++ zay.js
@@ -6,3 +6,22 @@ console.log(country, continent, population);
 let island = false;
 let language;
 console.log(typeof island, typeof population, typeof country);
+
+language = "Yoruba";
+const name = "Akpowu";
+const surname = "Arubi";
+
+console.log(population / 2);
+population++;
+console.log(population > 6);
+console.log(population < 33);
+
+const description = `${country} is in ${continent} and ${population} million people speak ${language}`;
+
+if (population > 33) {
+  console.log(`${country}'s population is above average`);
+} else {
+  console.log(
+    `${country}'s population is ${33 - population} million below average`
+  );
+}
